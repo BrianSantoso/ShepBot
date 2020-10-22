f7030076d4643696007d91794d45b83ffdb49234
Brian Santoso <brian.santoso@berkeley.edu>
2020-10-22 09:17:33 -0700

    cleaned code

--- bot.py
+++ bot.py
@@ -8,6 +8,8 @@ from pytz import timezone, utc
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+PIN_CHANNEL = 'pins-1'
+PIN_REACTS = '📌📍🧷📎'
 
 bot = commands.Bot(command_prefix='pin ')
 
@@ -15,15 +17,12 @@ bot = commands.Bot(command_prefix='pin ')
 async def on_ready():
     print(f'Bot {bot.user} has connected to Discord!')
 
-
-PIN_CHANNEL_PREFIX = 'pins'
-
 @bot.group()
 async def transfer(ctx):
 	if ctx.invoked_subcommand is None:
 		pass
 	pass
-	
+
 @bot.group()
 async def untransfer(ctx):
 	if ctx.invoked_subcommand is None:
@@ -37,10 +36,10 @@ async def all(ctx):
 	native_pins = await channel.pins()
 	count = 0
 	for message in native_pins:
-		success = await pin_to_category(guild, message)
+		success = await pin(guild, message)
 		count += int(success)
 
-	pins_channel = await safe_create_channel(guild, None, 1)
+	pins_channel = await get_pins_channel(guild)
 	await ctx.send(f'Transfered {count} native pins in {channel.name} to {pins_channel.name}')
 
 
@@ -52,46 +51,48 @@ async def all(ctx):
 	count = 0
 	native_pins = await channel.pins()
 	for message in native_pins:
-		pins_channel = await safe_create_channel(guild, None, 1)
-		matched_pin = await find_pin(pins_channel, message)
-		print(message, matched_pin)
-		if matched_pin:
-			await matched_pin.delete()
-			count += 1
+		success = await unpin(guild, message)
+		count += success
+
+	pins_channel = await get_pins_channel(guild)
 	await ctx.send(f'Untransfered {count} native pins in {channel.name} from {pins_channel.name}')
 
-async def pin_to_category(guild, message, category_name=''):
-	# category = await safe_create_category(guild, category_name)
+async def pin(guild, message, category_name=''):
 	category = discord.utils.get(guild.categories, name=category_name)
-	# category = None
 
-	async def pin_to_channel(index):
-		channel = await safe_create_channel(guild, category, index)
-		# await message.pin()
+	channel = await get_pins_channel(guild, category)
 
-		# dont pin if message is already pinned to this channel (determine by hash on date?)
-		description = create_description(message)
-		matched_pin = await find_pin(channel, message)
+	# Don't pin if message is already pinned to this channel (determine by hash on date?)
+	description = create_description(message)
+	matched_pin = await find_pin(channel, message)
 
-		if matched_pin:
-			print('matched pin ', matched_pin)
-			return False
-		
-		attachments = reduce(lambda urls, attachment: f'{urls}\n{attachment.url}', message.attachments, '')
-		author_tag = f'<@{message.author.id}>'
-		author_profile = f'https://discordapp.com/users/{message.author.id}'
-		pfp = message.author.avatar_url
-		# https://leovoel.github.io/embed-visualizer/
-		# https://stackoverflow.com/questions/51423859/get-profile-picture-from-set-user
-		# https://www.programcreek.com/python/example/107400/discord.Embed
-		embed = discord.Embed(title=message.content, description=description, color=0x00ff00)
-		embed.set_author(name=message.author, icon_url=pfp)
-		for url in attachments.split():
-			embed.set_image(url=url)
-		await channel.send(embed=embed)
+	if matched_pin:
+		return False
+	
+	attachments = reduce(lambda urls, attachment: f'{urls}\n{attachment.url}', message.attachments, '')
+	author_tag = f'<@{message.author.id}>'
+	author_profile = f'https://discordapp.com/users/{message.author.id}'
+	pfp = message.author.avatar_url
+
+	# https://leovoel.github.io/embed-visualizer/
+	# https://stackoverflow.com/questions/51423859/get-profile-picture-from-set-user
+	# https://www.programcreek.com/python/example/107400/discord.Embed
+	embed = discord.Embed(title=message.content, description=description, color=0x00ff00)
+	embed.set_author(name=message.author, icon_url=pfp)
+	for url in attachments.split():
+		embed.set_image(url=url)
+	await channel.send(embed=embed)
+
+	return True
+
+async def unpin(guild, message, category_name=''):
+	category = discord.utils.get(guild.categories, name=category_name)
+	channel = await get_pins_channel(guild, category)
+	matched_pin = await find_pin(channel, message)
+	if matched_pin:
+		await matched_pin.delete()
 		return True
-
-	return await pin_to_channel(1)
+	return False
 
 async def safe_create_category(guild, category_name):
 	print(guild, guild.categories)
@@ -101,8 +102,9 @@ async def safe_create_category(guild, category_name):
 		category = await guild.create_category(category_name)
 	return category
 
-async def safe_create_channel(guild, category, index):
-	channel_name = f'{PIN_CHANNEL_PREFIX}-{index}'
+# Get the pin channel in the guild, scoped optionally by category
+async def get_pins_channel(guild, category=None):
+	channel_name = f'{PIN_CHANNEL}'
 	if category is None:
 		# search all channels
 		channel = discord.utils.get(guild.channels, name=channel_name)
@@ -124,24 +126,21 @@ async def on_raw_reaction_add(payload):
 	guild = bot.get_guild(payload.guild_id)
 	channel = discord.utils.get(bot.get_all_channels(), id=payload.channel_id)
 	message = await channel.fetch_message(payload.message_id)
-	if str(payload.emoji) == "📌":
-		await pin_to_category(guild, message)
+	if str(payload.emoji) in PIN_REACTS:
+		await pin(guild, message)
 
 @bot.event
 async def on_raw_reaction_remove(payload):
 	guild = bot.get_guild(payload.guild_id)
 	channel = discord.utils.get(bot.get_all_channels(), id=payload.channel_id)
 	message = await channel.fetch_message(payload.message_id)
-	if str(payload.emoji) == "📌":
-		channel = await safe_create_channel(guild, None, 1)
-		if channel:
-			matched_pin = await find_pin(channel, message)
-			if matched_pin:
-				await matched_pin.delete()
+	if str(payload.emoji) in PIN_REACTS:
+		await unpin(guild, message)
 
 def get_message_link(message):
 	return f'https://discord.com/channels/{message.guild.id}/{message.channel.id}/{message.id}'
 
+# Create a message's signature which contains it's time and hyperlink
 def create_description(message):
 	message_link = get_message_link(message)
 	timestamp = message.created_at.replace(tzinfo=utc)
@@ -158,7 +157,6 @@ def create_description(message):
 	return description
 
 async def find_pin(channel, message):
-
 	history = await channel.history().flatten()
 	description = create_description(message)
 
@@ -176,4 +174,3 @@ async def find_pin(channel, message):
 # good example bot: https://github.com/HaiderZaidiDev/Discord-Pin-Archiver-Bot/blob/public-release-dev/main.py
 
 bot.run(TOKEN)
-
